0daa6b88eb1afde8ff97081de16b88aaff8a1c31
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-08-17 18:51:07 -0500

    reformatted with black

--- mlfinance/nlp/random_generator.py
+++ mlfinance/nlp/random_generator.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 This makes random data for training and stores it in mlfinance/nlp/data
 
@@ -28,8 +28,7 @@ pl-transformers-train \
 
 
 
-'''
-
+"""
 
 
 import os
@@ -37,13 +36,12 @@ import random
 from getpaths import getpath
 
 
-
 def random_text(num_characters: int = None) -> str:
-    '''
-returns random text of length num_characters
-    '''
+    """
+    returns random text of length num_characters
+    """
 
-    chars = ' abcdefghijklmnopqrstuvwxyz1234567890'
+    chars = " abcdefghijklmnopqrstuvwxyz1234567890"
     char_list = [char for char in chars]
 
     text_list = []
@@ -51,41 +49,47 @@ returns random text of length num_characters
     for i in range(num_characters):
         text_list.append(random.choice(char_list))
 
-    return ''.join(text_list)
-
+    return "".join(text_list)
 
 
 def json_random_sentence() -> str:
 
     num_characters = 30
-    labels = ['stock', 'general']
+    labels = ["stock", "general"]
 
-    sentence = '{\n'   +                                                       \
-               '    '  + '"label": "' + random.choice(labels)       + '",\n' + \
-               '    '  + '"text": "'  + random_text(num_characters) + '"\n'  + \
-               '},\n'
-    
+    sentence = (
+        "{\n"
+        + "    "
+        + '"label": "'
+        + random.choice(labels)
+        + '",\n'
+        + "    "
+        + '"text": "'
+        + random_text(num_characters)
+        + '"\n'
+        + "},\n"
+    )
+
     return sentence
-
 
 
 def csv_random_sentence() -> str:
 
     num_characters = 30
-    labels = ['stock', 'general']
+    labels = ["stock", "general"]
 
-    sentence = '' + random.choice(labels)       + ','  + \
-               '' + random_text(num_characters) + '\n'
+    sentence = (
+        "" + random.choice(labels) + "," + "" + random_text(num_characters) + "\n"
+    )
 
     return sentence
 
 
-
 def make_json_data() -> None:
     cwd = getpath()
-    
-    data_directory = cwd/'data'
-    
+
+    data_directory = cwd / "data"
+
     # check if path exists
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
@@ -93,23 +97,23 @@ def make_json_data() -> None:
     num_sentences = 10
 
     # beginning of file
-    sentences = ['{\n']
+    sentences = ["{\n"]
 
     for i in range(num_sentences):
         sentences.append(json_random_sentence())
-    
-    # end of file
-    sentences.append('}')
 
-    with open(data_directory/'train.json', 'w') as file_handler:
-        file_handler.write(''.join(sentences))
+    # end of file
+    sentences.append("}")
+
+    with open(data_directory / "train.json", "w") as file_handler:
+        file_handler.write("".join(sentences))
 
 
 def make_csv_data() -> None:
     cwd = getpath()
-    
-    data_directory = cwd/'data'
-    
+
+    data_directory = cwd / "data"
+
     # check if path exists
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
@@ -117,14 +121,14 @@ def make_csv_data() -> None:
     num_sentences = 10
 
     # beginning of file
-    sentences = ['label,text\n']
+    sentences = ["label,text\n"]
 
     for i in range(num_sentences):
         sentences.append(csv_random_sentence())
 
-    with open(data_directory/'train.csv', 'w') as file_handler:
-        file_handler.write(''.join(sentences))
+    with open(data_directory / "train.csv", "w") as file_handler:
+        file_handler.write("".join(sentences))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     make_csv_data()
