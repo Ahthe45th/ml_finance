4bf0c78fc37999631cab1f5d2966a3320fed6ff9
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-08-24 06:31:15 -0500

    manually put in training loop for DistilBert, will have to change for the future but works for now

--- mlfinance/nlp/bert/model.py
+++ mlfinance/nlp/bert/model.py
@@ -1,7 +1,7 @@
 from transformers import get_linear_schedule_with_warmup, AdamW
-from transformers import BertModel
 import pytorch_lightning as pl
 import torch.nn as nn
+import transformers
 import torch
 
 
@@ -11,19 +11,39 @@ class BertBaseModel(pl.LightningModule):
         n_classes: int = None,
         n_training_steps: int = None,
         n_warmup_steps: int = None,
-        name: str = "bert-base-cased",
+        model_id: str = "bert-base-cased",
+        **kwargs
     ):
         super().__init__()
-        self.name = name
-        self.bert = BertModel.from_pretrained(name, return_dict=True)
+        self.kwargs = kwargs
+        
+        self.model_id = model_id
+        if 'transformer_module' in kwargs:
+            self.bert = eval(f"transformers.{self.kwargs['transformer_module']}.from_pretrained(model_id, return_dict=True)")
+        else:
+            self.bert = transformers.BertModel.from_pretrained(model_id, return_dict=True)
+
+        self.pre_classifier = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
         self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
         self.n_training_steps = n_training_steps
         self.n_warmup_steps = n_warmup_steps
         self.criterion = nn.BCELoss()
 
     def forward(self, input_ids, attention_mask, labels=None):
-        output = self.bert(input_ids, attention_mask=attention_mask)
-        output = self.classifier(output.pooler_output)
+        output = self.bert(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+
+        try:
+            output = self.classifier(output.pooler_output)
+        except:
+            # Right way to get hidden state from DistilBert
+            # https://huggingface.co/transformers/_modules/transformers/modeling_distilbert.html
+            hidden_state = output[0]  # (bs, seq_len, dim)
+            pooled_output = hidden_state[:, 0]  # (bs, dim)
+            pooled_output = self.pre_classifier(pooled_output)  # (bs, dim)
+            pooled_output = nn.ReLU()(pooled_output)  # (bs, dim)
+            # pooled_output = self.dropout(pooled_output)  # (bs, dim)
+            output = self.classifier(pooled_output)
+
         output = torch.sigmoid(output)
         loss = 0
         if labels is not None:
@@ -44,7 +64,8 @@ class BertBaseModel(pl.LightningModule):
         labels = batch["labels"]
         loss, outputs = self(input_ids, attention_mask, labels)
         self.log("val_loss", loss, prog_bar=True, logger=True)
-        return loss
+        return {"loss": loss, "predictions": outputs, "labels": labels}
+    
 
     def test_step(self, batch, batch_idx):
         input_ids = batch["input_ids"]
@@ -52,7 +73,7 @@ class BertBaseModel(pl.LightningModule):
         labels = batch["labels"]
         loss, outputs = self(input_ids, attention_mask, labels)
         self.log("test_loss", loss, prog_bar=True, logger=True)
-        return loss
+        return {"loss": loss, "predictions": outputs, "labels": labels}
 
     def training_epoch_end(self, outputs):
 
