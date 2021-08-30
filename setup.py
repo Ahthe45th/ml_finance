9c30f8cd9060f29c1c6327716a1ddfb52606800d
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-08-29 22:25:06 -0500

    accepted changes to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ from setuptools import setup, find_packages
 os.system('pip install pipreqs')
 currentdir = os.path.dirname(os.path.realpath(__file__))
 os.system(f'pipreqs {currentdir}')
-<<<<<<< HEAD
-
-=======
-#os.system()
->>>>>>> 22998ebf8b4b897ea1cb3b794cb94c88fb007676
 reqs = open('requirements.txt').read().split('\n')
 reqs = [x for x in reqs if 'yusuf' not in x]
 
